c833bd7c1941429c65eaccae221d6eb9a2b8d2a2
John Manhart <manhartjohn@gmail.com>
2017-01-19 08:36:37 -0600

    Production changing name and stuff

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import ExampleTwoDeepComponent from './components/ExampleTwoDeepComponent';
 // This will not be an issue cause still deving dingus
 
 const routes = (
-  <Route path="/" mapMenuTitle="Home" component={App}>
+  <Route path="/interactions" mapMenuTitle="Home" component={App}>
     <IndexRoute component={Home} />
 
     {/*<Route path="/example" mapMenuTitle="Example" component={ExampleComponent}>
