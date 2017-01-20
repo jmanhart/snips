ab5b386cd36f00fd58aabc1526ec201fef719c6b
John Manhart <manhartjohn@gmail.com>
2017-01-19 21:19:54 -0600

    no message

--- src/index.js
+++ src/index.js
@@ -11,19 +11,12 @@ import CardsInteraction from './containers/cards-interaction';
 // This will not be an issue cause still deving dingus
 
 const routes = (
-<<<<<<< HEAD
-=======
-  <Route path="/interactions" mapMenuTitle="Home" component={App}>
-    <IndexRoute />
->>>>>>> devlopment
-
   <Route path="/interactions" mapMenuTitle="Home" component={App}>
     <Route path="/msg" mapMenuTitle="Example" component={MessageInteraction} />
     <Route path="/cards" mapMenuTitle="Example" component={CardsInteraction} />
 
   </Route>
 );
-
 
 render(
   <Router
